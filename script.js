ee5e21df8b7129d45ee07bce88c6ef4578e8089b
Esentai <1.koktalov@gmail.com>
2023-12-04 20:07:22 +0600

    02.12

---
+++ script.js
@@ -0,0 +1,48 @@
+/*
+let age = 28;
+if(age < 18){
+    let message = "Вам больше 18 лет";
+    alert(message);
+} else {
+    alert("Test");
+}
+*/
+
+
+/*
+let salary = 600;
+if(salary < 300){
+    console.log('Ниже среднего');
+} else if(salary >= 300 && salary <= 300){
+    console.log('Средний');
+} else {
+    console.log('not undefined');
+}
+*/
+
+
+//тернарный оператор ?
+/*
+const age = 17;
+const success = age > 18 ? 'Разрещен' : 'Запрещен';
+console.log(success);
+*/
+
+
+//true or false
+/*
+let value = '';
+
+if(value){
+    console.log(value);
+}
+*/
+
+
+var userInput = prompt("Столица Кыргызстана?");
+
+if (userInput === "Бишкек") {
+  alert("Верно");
+} else {
+  alert("Не знаете?");
+}
